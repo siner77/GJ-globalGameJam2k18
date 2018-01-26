fabb9acad514281d7efb1056f03640ecd9c81d5b
SineR <patryk.kochanski@hotmail.com>
2018-01-26 22:34:30 +0100

    Adding almost empty palnete and satellite classes

---
+++ Assets/Scripts/Planete.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planete : MonoBehaviour {
+
+    private float _rotationParameter = 5.0f;
+    private ArrayList<Satellite> _satellistes = new ArrayList<Satellite>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.transform.Rotate(new Vector3(0f, 0f, Time.deltaTime * _rotationParameter));
+	}
+}
