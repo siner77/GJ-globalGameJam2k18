19056d50dfba73dfb64b55eb88d54208366e1821
kstengert@MSI_GE40 <krzysztof.stengert@hotmail.com>
2018-01-28 00:19:11 +0100

    [+] Determining starting planet and using it to spawn ally ships [*] After destroying all satellites the enemy should fly to next planet [*] Fixes in ships prefabs [*] Removed Planet layer from Antenne layer mask in PlanetEmmiter prefab

--- Assets/Scripts/Cards/ShipCard.cs
+++ Assets/Scripts/Cards/ShipCard.cs
@@ -8,11 +8,13 @@ public class ShipCard : SelectableCard<Planet>
     private ShipController _shipPrefab;
 
     private SpawnPool<ShipController> _shipSpawnPool;
+    private LevelManager _levelManager;
 
     private void OnEnable()
     {
         Init(LayerMask.GetMask("Planet"));
         _shipSpawnPool = new SpawnPool<ShipController>(_shipPrefab, 10);
+        _levelManager = FindObjectOfType<LevelManager>();
     }
 
     protected override void OnTargetClicked(Planet target)
@@ -23,8 +25,15 @@ public class ShipCard : SelectableCard<Planet>
             return;
         }
 
-        // TODO: Determine spawn position
+        Planet startingPlanet = _levelManager.GetStartingPlanet();
+        if(startingPlanet == null || !startingPlanet.HasUnusedAllySpot())
+        {
+            return;
+        }
+
         ShipController spawnedShip = _shipSpawnPool.Get();
+        Spot unusedSpot = startingPlanet.GetRandomUnusedAllySpot();
+        spawnedShip.transform.position = unusedSpot.SpotTransform.position;
         spawnedShip.SetState(new ShipStates.GoToPlanet(target, new ShipStates.Defend(target)));
     }
 }
