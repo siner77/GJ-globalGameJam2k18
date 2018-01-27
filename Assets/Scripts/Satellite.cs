2f0c6385909f11767ed012f19cbb70dfe5379ba2
SineR <patryk.kochanski@hotmail.com>
2018-01-27 16:33:06 +0100

    Merge fix

--- Assets/Scripts/Satellite.cs
+++ Assets/Scripts/Satellite.cs
@@ -13,13 +13,27 @@ public class Satellite : Emitter
         set { _anchoredPlanet = value; }
     }
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private float _maxHP;
+    [SerializeField]
+    private float _armor;
+
+    private float _currentHP;
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 
-	// Update is called once per frame
-	void Update () {
+    private void OnEnable()
+    {
+        _currentHP = _maxHP;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -43,6 +57,22 @@ public class Satellite : Emitter
     public void OnGettingSignalEnd()
     {
         _signalPlaneObject.SetActive(false);
+    }
+
+
+    public bool IsAlive()
+    {
+        return gameObject.activeInHierarchy;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHP -= Mathf.Max(damage - _armor, 0.0f);
+        if (_currentHP <= 0.0f)
+        {
+            gameObject.SetActive(false);
+            AnchoredPlanet.RemoveSatellite(this);
+        }
     }
 
 }
