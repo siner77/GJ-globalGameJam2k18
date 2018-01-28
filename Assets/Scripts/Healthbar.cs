787cd10d53c9bbec3e7bef4ed8bfa09587e2fae6
kstengert@MSI_GE40 <krzysztof.stengert@hotmail.com>
2018-01-28 13:17:03 +0100

    [+] HealthBar

---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField]
+    private float _xScaleMultiplier;
+
+    private ShipController _shipController;
+    private SpriteRenderer _healthBar;
+
+    private void Start()
+    {
+        _shipController = GetComponentInParent<ShipController>();
+    }
+
+    private void Update()
+    {
+        _healthBar.transform.localScale = new Vector3(_shipController.GetHealthPercent() * _xScaleMultiplier, _healthBar.transform.localScale.y, _healthBar.transform.localScale.z);
+    }
+}
